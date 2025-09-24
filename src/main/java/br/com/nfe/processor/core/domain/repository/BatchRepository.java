29f9efea4859b9efeced6eee926ae3600caa9ef3
DANIEL LAU PEREIRA SOARES <danniellau@gmail.com>
2025-09-24 17:36:08 -0300

    feat: add dashboard listing and ui

--- src/main/java/br/com/nfe/processor/core/domain/repository/BatchRepository.java
+++ src/main/java/br/com/nfe/processor/core/domain/repository/BatchRepository.java
@@ -2,6 +2,8 @@ package br.com.nfe.processor.core.domain.repository;
 
 import br.com.nfe.processor.core.domain.model.Batch;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +11,7 @@ public interface BatchRepository extends JpaRepository<Batch, String> {
 
     @EntityGraph(attributePaths = {"invoices", "issues", "validations", "invoices.issues"})
     Optional<Batch> findWithDetailsById(String id);
+
+    @EntityGraph(attributePaths = {"issues"})
+    Page<Batch> findAllBy(Pageable pageable);
 }
